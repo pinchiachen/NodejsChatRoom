5caefdc503ae2b586c672fadce2d36f36d8f803e
pinchiachen <chenargar@gmail.com>
2019-12-29 20:16:35 +0800

    update db-connector address

--- db-connector.js
+++ db-connector.js
@@ -2,7 +2,8 @@ const setting = require('./setting.js');
 const mongoose = require('mongoose');
 var name = setting.getName();
 var passwd = setting.getPasswd();
-const uri = "mongodb+srv://" + name + ":" + passwd + "@nodechatroom-zj1ib.gcp.mongodb.net/test?retryWrites=true&w=majority"
+var address = setting.getAddress();
+const uri = "mongodb+srv://" + name + ":" + passwd + address
 mongoose.connect(uri);
 
 module.exports = mongoose.connection;
